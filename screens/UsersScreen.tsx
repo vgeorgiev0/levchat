f009b0c82acea25e73261400878fc0e06ecc339b
Valentin Georgiev <v.georgiev@up2technology.com>
2022-03-10 16:38:59 +0200

    Query Users from backend

--- screens/UsersScreen.tsx
+++ screens/UsersScreen.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
+import { DataStore } from '@aws-amplify/datastore';
 import ChatRoomData from '../assets/SignalAssets/dummy-data/Users';
 import UserItem from '../components/UserItem';
+import { User } from '../src/models';
+import { useState, useEffect } from 'react';
 
 type Props = {};
 
 const UsersScreen = (props: Props) => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    DataStore.query(User).then(setUsers);
+  }, []);
+
+  // useEffect(() => {
+  //   // Query users
+  //   const fetchUsers = async () => {
+  //     const fetchedUsers = await DataStore.query(User);
+  //     setUsers(fetchedUsers);
+  //   };
+  //   fetchUsers();
+  // }, []);
+
   return (
     <View style={styles.page}>
       <FlatList
-        data={ChatRoomData}
+        data={users}
         renderItem={(props) => <UserItem user={props.item} />}
         showsVerticalScrollIndicator={false}
       />
