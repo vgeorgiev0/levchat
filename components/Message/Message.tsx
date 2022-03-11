b472289fbf8c4a100b27aadba7879139c8ee8663
Valentin Georgiev <v.georgiev@up2technology.com>
2022-03-11 16:03:42 +0200

    Query and send messages

--- components/Message/Message.tsx
+++ components/Message/Message.tsx
@@ -1,11 +1,37 @@
-import { Text, View } from 'react-native';
-import React from 'react';
-import styles from './styles';
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
+import { DataStore } from "@aws-amplify/datastore";
+import { User } from "../../src/models";
+import styles from "./styles";
+import { Auth } from "aws-amplify";
 
-const myID = 'u1';
+const myID = "u1";
 
 const Message = ({ message }: any) => {
-  const isMe = message.user.id === myID;
+  const [user, setUser] = useState<User | undefined>();
+  const [isMe, setIsMe] = useState<boolean>(false);
+
+  useEffect(() => {
+    DataStore.query(User, message.userID).then(setUser);
+  }, []);
+
+  useEffect(() => {
+    const checkIsMe = async () => {
+      if (!user) {
+        return;
+      }
+
+      const authUser = await Auth.currentAuthenticatedUser();
+      setIsMe(user?.id === authUser.attributes.sub);
+    };
+    checkIsMe();
+  }, [user]);
+
+  if (!user) {
+    return <ActivityIndicator />;
+  }
+
+  // const isMe = message.user.id === myID;
 
   return (
     <View
@@ -14,7 +40,7 @@ const Message = ({ message }: any) => {
         isMe ? styles.containerRight : styles.containerLeft,
       ]}
     >
-      <Text style={{ color: isMe ? 'black' : '#fff' }}>{message.content}</Text>
+      <Text style={{ color: isMe ? "black" : "#fff" }}>{message.content}</Text>
     </View>
   );
 };
--- components/MessageInput/MessageInput.tsx
+++ components/MessageInput/MessageInput.tsx
@@ -4,28 +4,39 @@ import {
   View,
   KeyboardAvoidingView,
   Platform,
-} from 'react-native';
-import React from 'react';
+} from "react-native";
+import React from "react";
 import {
   MaterialCommunityIcons,
   FontAwesome5,
   Feather,
   MaterialIcons,
-} from '@expo/vector-icons';
-import styles from './styles';
-import { useState } from 'react';
+} from "@expo/vector-icons";
+import { DataStore } from "@aws-amplify/datastore";
+import { Message } from "../../src/models";
+import styles from "./styles";
+import { useState } from "react";
+import { Auth } from "aws-amplify";
 
 type Props = {};
 
-const MessageInput = (props: Props) => {
-  const [message, setMessage] = useState('');
+const MessageInput = ({ chatRoomId }) => {
+  const [message, setMessage] = useState("");
 
-  const sendMessage = () => {
-    console.log(`Sending: ${message}`);
-    setMessage('');
+  const sendMessage = async () => {
+    const user = await Auth.currentAuthenticatedUser();
+    await DataStore.save(
+      new Message({
+        content: message,
+        userID: user.attributes.sub,
+        chatroomID: chatRoomId,
+      })
+    );
+    // console.log(`Sending: ${message}`);
+    // setMessage("");
   };
   const onPlusClicked = () => {
-    console.log('Plus clicked');
+    console.log("Plus clicked");
   };
 
   const onPress = () => {
@@ -39,37 +50,37 @@ const MessageInput = (props: Props) => {
   return (
     <KeyboardAvoidingView
       style={styles.root}
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
       keyboardVerticalOffset={100}
     >
       <View style={styles.inputContainer}>
         <TouchableOpacity>
           <FontAwesome5
-            name='smile-wink'
+            name="smile-wink"
             size={24}
-            color='#595959'
+            color="#595959"
             style={styles.icon}
           />
         </TouchableOpacity>
         <TextInput
           value={message}
           style={styles.input}
-          placeholder='Send a message...'
+          placeholder="Send a message..."
           onChangeText={setMessage}
         />
         <TouchableOpacity>
           <Feather
-            name='camera'
+            name="camera"
             size={24}
-            color='#595959'
+            color="#595959"
             style={styles.icon}
           />
         </TouchableOpacity>
         <TouchableOpacity>
           <MaterialCommunityIcons
-            name='microphone-outline'
+            name="microphone-outline"
             size={24}
-            color='#595959'
+            color="#595959"
             style={styles.icon}
           />
         </TouchableOpacity>
@@ -77,9 +88,9 @@ const MessageInput = (props: Props) => {
       <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
         <View>
           {message ? (
-            <MaterialIcons name='send' size={18} color='white' />
+            <MaterialIcons name="send" size={18} color="white" />
           ) : (
-            <Feather name='plus' size={20} color='white' />
+            <Feather name="plus" size={20} color="white" />
           )}
         </View>
       </TouchableOpacity>
