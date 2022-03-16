1130bacbc681ef715bdaa3cc8708ea4145493f8e
Valentin Georgiev <v.georgiev@up2technology.com>
2022-03-16 12:12:57 +0200

    Record audio & display audio progress status

--- components/MessageInput/MessageInput.tsx
+++ components/MessageInput/MessageInput.tsx
@@ -22,6 +22,7 @@ import { Auth, Storage } from 'aws-amplify';
 import { ChatRoom } from '../../src/models';
 import EmojiSelector from 'react-native-emoji-selector';
 import * as ImagePicker from 'expo-image-picker';
+import { Audio, AVPlaybackStatus } from 'expo-av';
 import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -31,18 +32,28 @@ const MessageInput = ({ chatRoom }) => {
   const [isEmojiPickerOpen, setIsEmojiPickerOpen] = useState(false);
   const [image, setImage] = useState<string | null>(null);
   const [progress, setProgress] = useState(0);
+  const [recording, setRecording] = useState<Audio.Recording | null>(null);
+  const [soundURI, setSoundURI] = useState<Audio.Sound | null>(null);
+  const [paused, setPaused] = useState(true);
+  const [audioProgress, setAudioProgress] = useState(0);
 
   useEffect(() => {
-    async () => {
+    (async () => {
       if (Platform.OS !== 'web') {
+        const libraryResponse =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
         const photoResponse = await ImagePicker.requestCameraPermissionsAsync();
-        if (photoResponse.status !== 'granted') {
+        await Audio.requestPermissionsAsync();
+
+        if (
+          libraryResponse.status !== 'granted' ||
+          photoResponse.status !== 'granted'
+        ) {
           alert('Sorry, we need camera roll permissions to make this work!');
         }
       }
-    };
+    })();
   }, []);
-
   const sendMessage = async () => {
     // send message
     const user = await Auth.currentAuthenticatedUser();
@@ -156,6 +167,69 @@ const MessageInput = ({ chatRoom }) => {
     return blob;
   };
 
+  // Audio record
+
+  const onPlaybackStatusUpdate = (status: AVPlaybackStatus) => {
+    if (!status.isLoaded) {
+      return;
+    }
+    setAudioProgress(status.positionMillis / (status.durationMillis || 1));
+  };
+
+  async function startRecording() {
+    try {
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    if (!recording) {
+      return;
+    }
+    setRecording(null);
+    await recording.stopAndUnloadAsync();
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+    });
+    const uri = recording.getURI();
+    console.log('Recording stopped and stored at', uri);
+
+    if (!uri) {
+      return;
+    }
+    const { sound } = await Audio.Sound.createAsync(
+      { uri },
+      {},
+      onPlaybackStatusUpdate
+    );
+    setSoundURI(sound);
+  }
+  const playPauseSound = async () => {
+    if (!soundURI) {
+      return;
+    }
+    if (paused) {
+      setPaused(false);
+      await soundURI.playAsync();
+    } else {
+      setPaused(true);
+      await soundURI.pauseAsync();
+    }
+  };
+
   return (
     <KeyboardAvoidingView
       style={[styles.root, { height: isEmojiPickerOpen ? '50%' : 'auto' }]}
@@ -163,7 +237,7 @@ const MessageInput = ({ chatRoom }) => {
       keyboardVerticalOffset={100}
     >
       {image && (
-        <View style={styles.sentImageContainer}>
+        <View style={styles.sendContainer}>
           <Image
             source={{ uri: image }}
             style={{ width: 100, height: 100, borderRadius: 10 }}
@@ -185,6 +259,24 @@ const MessageInput = ({ chatRoom }) => {
               style={{ margin: 5 }}
             />
           </TouchableOpacity>
+        </View>
+      )}
+
+      {soundURI && (
+        <View
+          style={[styles.sendContainer, { padding: 10, alignItems: 'center' }]}
+        >
+          <TouchableOpacity onPress={playPauseSound}>
+            <Feather name={paused ? 'play' : 'pause'} size={24} color="grey" />
+          </TouchableOpacity>
+          <View style={styles.audioProgressBG}>
+            <TouchableOpacity
+              style={[
+                styles.audioProgressFG,
+                { left: `${audioProgress * 100}%` },
+              ]}
+            />
+          </View>
         </View>
       )}
 
@@ -224,11 +316,14 @@ const MessageInput = ({ chatRoom }) => {
               style={styles.icon}
             />
           </TouchableOpacity>
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPressIn={startRecording}
+            onPressOut={stopRecording}
+          >
             <MaterialCommunityIcons
-              name="microphone-outline"
+              name={recording ? 'microphone' : 'microphone-outline'}
               size={24}
-              color="#595959"
+              color={recording ? 'red' : '#595959'}
               style={styles.icon}
             />
           </TouchableOpacity>
